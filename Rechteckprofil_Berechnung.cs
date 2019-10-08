139f2c88cbfeb627d4dc6902b154459583e049fc
he2721 <hendrik.stoever@student.jade-hs.de>
2019-10-08 11:42:50 +0200

    Create Rechteckprofil_Berechnung.cs

---
+++ Rechteckprofil_Berechnung.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestBerechnung
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Laenge, Breite, Hoehe, Dichte;
+
+            Console.WriteLine("Geben Sie die Länge des Rechteckes in [m] ein:");
+            Laenge = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("Geben Sie die Breite des Rechteckes in [m] ein:");
+            Breite = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("Geben Sie die Höhe des Rechteckes in [m] ein:");
+            Hoehe = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("Geben Sie die Dichte des Werkstoffes in [kg/m³] ein:");
+            Dichte = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("Flächeninhalt: " + (Laenge * Breite) + "m²");
+            Console.WriteLine("Volumen: " + (Laenge * Breite * Hoehe) + "m³");
+            Console.WriteLine("Gewicht: " + (Laenge * Breite * Hoehe * Dichte) + "kg");
+
+            Console.ReadLine();
+            
+        }
+    }
+}
