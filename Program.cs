9f472ad5f53aff491ad4f4363255c45e6121d51e
fr2562 <56021527+fr2562@users.noreply.github.com>
2019-10-22 10:25:23 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,13 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace Profilberechnung
 {
     class Program
     {
         static void Main(string[] args)
-        { Console.WriteLine("Hello World");
-            Console.ReadKey();
+        {
         }
     }
 }
