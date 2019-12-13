53c275a8c2f063cf56806bb12e6744862637f4cb
Frerk <frerk.lange@web.de>
2019-12-13 11:26:49 +0100

    Fehler

---
+++ Vierkantrohr/Catia_Anbindung_GUI/CatiaCon.cs
@@ -0,0 +1,330 @@
+﻿using INFITF;
+using MECMOD;
+using PARTITF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Catia_Anbindung_GUI
+{
+    internal class CatiaCon
+    {
+        INFITF.Application hsp_catiaApp;
+        MECMOD.PartDocument hsp_catiaPart;
+        MECMOD.Sketch hsp_catiaProfil;
+        public bool CATIALaeuft()
+        {
+            try
+            {
+                object catiaObject = System.Runtime.InteropServices.Marshal.GetActiveObject(
+                   "CATIA.Application");
+                hsp_catiaApp = (INFITF.Application)catiaObject;
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public Boolean ErzeugePart()
+        {
+            INFITF.Documents catDocuments1 = hsp_catiaApp.Documents;
+            hsp_catiaPart = catDocuments1.Add("Part") as MECMOD.PartDocument;
+            return true;
+        }
+        public void ErstelleLeereSkizze()
+        {
+            HybridBody catHybridBody1;
+            HybridBodies catHybridBodies1;
+            try
+            {
+                catHybridBodies1 = hsp_catiaPart.Part.HybridBodies;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Was auch immer");
+                return;
+            }
+
+            try
+            {
+                catHybridBody1 = catHybridBodies1.Item("Geometrisches Set.1");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Kein geometrisches Set gefunden! " + Environment.NewLine +
+                       "Ein PART manuell erzeugen und ein darauf achten, dass 'Geometisches Set' aktiviert ist.",
+                       "Fehler", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+            catHybridBody1.set_Name("Profile");
+            Sketches catSketches1 = catHybridBody1.HybridSketches;
+            OriginElements catOriginElements = hsp_catiaPart.Part.OriginElements;
+            Reference catReference1 = (Reference)catOriginElements.PlaneYZ;
+            hsp_catiaProfil = catSketches1.Add(catReference1);
+            ErzeugeAchsensystem();
+            hsp_catiaPart.Part.Update();
+        }
+        private void ErzeugeAchsensystem()
+        {
+            object[] arr = new object[]
+            {
+                0.0, 0.0, 0.0,
+                0.0, 1.0, 0.0,
+                0.0, 0.0, 1.0
+            };
+            hsp_catiaProfil.SetAbsoluteAxisData(arr);
+        }
+
+        public void ErzeugeKreis(Double r)
+        {
+            hsp_catiaProfil.set_Name("Kreis");
+            Factory2D catFactory2D1 = hsp_catiaProfil.OpenEdition();
+
+            Point2D catPoint2D1 = catFactory2D1.CreatePoint(0,0);
+            Circle2D catcircle2D1 = catFactory2D1.CreateClosedCircle(0.000000, 0.000000,r);
+            catcircle2D1.CenterPoint = catPoint2D1;
+
+            hsp_catiaProfil.CloseEdition();
+            hsp_catiaPart.Part.Update();
+        }
+
+        public void ErzeugeKreisring(Double ra, Double ri)
+        {
+            hsp_catiaProfil.set_Name("Kreisring");
+            Factory2D catFactory2D1 = hsp_catiaProfil.OpenEdition();
+
+            Point2D catPoint2D1 = catFactory2D1.CreatePoint(0, 0);
+            Circle2D catcircle2D1 = catFactory2D1.CreateClosedCircle(0.000000, 0.000000, ra);
+            Circle2D catcircle2D2 = catFactory2D1.CreateClosedCircle(0.000000, 0.000000, ri);
+            catcircle2D1.CenterPoint = catPoint2D1;
+
+            hsp_catiaProfil.CloseEdition();
+            hsp_catiaPart.Part.Update();
+        }
+
+        public void ErzeugeRechteck(Double h, Double b)
+        {
+            hsp_catiaProfil.set_Name("Rechteck");
+            Factory2D catFactory2D1 = hsp_catiaProfil.OpenEdition();
+
+            Point2D catPoint2D1 = catFactory2D1.CreatePoint(0,h);
+            Point2D catPoint2D2 = catFactory2D1.CreatePoint(b,h);
+            Point2D catPoint2D3 = catFactory2D1.CreatePoint(b,0);
+            Point2D catPoint2D4 = catFactory2D1.CreatePoint(0,0);
+
+            Line2D catLine2D1 = catFactory2D1.CreateLine(0,h,b,h);
+            catLine2D1.StartPoint = catPoint2D1;
+            catLine2D1.EndPoint = catPoint2D2;
+
+            Line2D catLine2D2 = catFactory2D1.CreateLine(b,h,b,0);
+            catLine2D2.StartPoint = catPoint2D2;
+            catLine2D2.EndPoint = catPoint2D3;
+
+            Line2D catLine2D3 = catFactory2D1.CreateLine(b,0,0,0);
+            catLine2D3.StartPoint = catPoint2D3;
+            catLine2D3.EndPoint = catPoint2D4;
+
+            Line2D catLine2D4 = catFactory2D1.CreateLine(0,0,0,h);
+            catLine2D4.StartPoint = catPoint2D4;
+            catLine2D4.EndPoint = catPoint2D1;
+
+            hsp_catiaProfil.CloseEdition();
+            hsp_catiaPart.Part.Update();
+        }
+
+        public void ErzeugeVierkantrohr(Double r_ah, Double r_ih, Double r_ab, Double r_ib)
+        {
+            hsp_catiaProfil.set_Name("VierkantRohr");
+            Factory2D catFactory2D1 = hsp_catiaProfil.OpenEdition();
+            double z = r_ab - r_ib;
+            double x = r_ah - r_ih;
+
+            Point2D catPoint2D1 = catFactory2D1.CreatePoint(z, z);
+            Point2D catPoint2D2 = catFactory2D1.CreatePoint(z, r_ah-z);
+            Point2D catPoint2D3 = catFactory2D1.CreatePoint(r_ab-z, r_ah-z);
+            Point2D catPoint2D4 = catFactory2D1.CreatePoint(r_ab,z);
+
+            Point2D catPoint2D5 = catFactory2D1.CreatePoint(z/ 2, r_ih + x / 2);
+            Point2D catPoint2D6 = catFactory2D1.CreatePoint(r_ib + z/ 2, r_ih + x/ 2);
+            Point2D catPoint2D7 = catFactory2D1.CreatePoint(r_ib + z/ 2, x/ 2);
+            Point2D catPoint2D8 = catFactory2D1.CreatePoint(z/ 2,x/ 2);
+
+            Point2D catPoint2D9 = catFactory2D1.CreatePoint(0, r_ah -z);
+            Point2D catPoint2D10 = catFactory2D1.CreatePoint(z,r_ah -z);
+
+            Point2D catPoint2D11 = catFactory2D1.CreatePoint(r_ab, r_ah-z);
+            Point2D catPoint2D12 = catFactory2D1.CreatePoint(r_ab-z, r_ah-z);
+
+            Point2D catPoint2D13 = catFactory2D1.CreatePoint(r_ab,z);
+            Point2D catPoint2D14 = catFactory2D1.CreatePoint(r_ab-z, z);
+
+            Point2D catPoint2D15 = catFactory2D1.CreatePoint(z, z);
+            Point2D catPoint2D16 = catFactory2D1.CreatePoint(z, 0);
+
+            Circle2D catCircle2D1 = catFactory2D1.CreateCircle(z, r_ah - z, z, z, z);
+            catCircle2D1.CenterPoint = catPoint2D10;
+            catCircle2D1.StartPoint = catPoint2D9;
+            catCircle2D1.EndPoint = catPoint2D1;
+
+            Circle2D catCircle2D2 = catFactory2D1.CreateCircle(r_ab-z, r_ah-z,z, z, z);
+            catCircle2D2.CenterPoint = catPoint2D12;
+            catCircle2D2.StartPoint = catPoint2D2;
+            catCircle2D2.EndPoint = catPoint2D11;
+
+            Circle2D catCircle2D3 = catFactory2D1.CreateCircle(r_ab-z,z, z, z, z);
+            catCircle2D2.CenterPoint = catPoint2D14;
+            catCircle2D2.StartPoint = catPoint2D13;
+            catCircle2D2.EndPoint = catPoint2D3;
+
+            Circle2D catCircle2D4 = catFactory2D1.CreateCircle(z, z, z, z, z);
+            catCircle2D2.CenterPoint = catPoint2D15;
+            catCircle2D2.StartPoint = catPoint2D16;
+            catCircle2D2.EndPoint = catPoint2D4;
+
+            Line2D catLine2D1 = catFactory2D1.CreateLine(z, r_ah, r_ab-z, r_ah);
+            catLine2D1.StartPoint = catPoint2D1;
+            catLine2D1.EndPoint = catPoint2D2;
+
+            Line2D catLine2D2 = catFactory2D1.CreateLine(r_ab, r_ah-z, r_ab,z);
+            catLine2D2.StartPoint = catPoint2D11;
+            catLine2D2.EndPoint = catPoint2D13;
+
+            Line2D catLine2D3 = catFactory2D1.CreateLine(r_ab-z, 0, z, 0);
+            catLine2D3.StartPoint = catPoint2D3;
+            catLine2D3.EndPoint = catPoint2D16;
+
+            Line2D catLine2D4 = catFactory2D1.CreateLine(0, z, 0, r_ah-z);
+            catLine2D4.StartPoint = catPoint2D4;
+            catLine2D4.EndPoint = catPoint2D9;
+
+            Line2D catLine2D5 = catFactory2D1.CreateLine(z/ 2, x/ 2, r_ib +z/ 2, r_ih +x/ 2);
+            catLine2D5.StartPoint = catPoint2D5;
+            catLine2D5.EndPoint = catPoint2D6;
+
+            Line2D catLine2D6 = catFactory2D1.CreateLine(r_ib + z/ 2, r_ih + x/ 2, r_ib + z/ 2, x/ 2);
+            catLine2D6.StartPoint = catPoint2D6;
+            catLine2D6.EndPoint = catPoint2D7;
+
+            Line2D catLine2D7 = catFactory2D1.CreateLine(r_ib + z / 2, x / 2, z/ 2, x/ 2);
+            catLine2D7.StartPoint = catPoint2D7;
+            catLine2D7.EndPoint = catPoint2D8;
+
+            Line2D catLine2D8 = catFactory2D1.CreateLine(z / 2, x/ 2, z/ 2, r_ih + x / 2);
+            catLine2D8.StartPoint = catPoint2D8;
+            catLine2D8.EndPoint = catPoint2D5;
+
+            hsp_catiaProfil.CloseEdition();
+            hsp_catiaPart.Part.Update();
+        }
+        public void ErzeugeRechteckRohr(Double r_ah, Double r_ih,Double r_ab,Double r_ib)
+        {
+            hsp_catiaProfil.set_Name("RechteckRohr");
+            Factory2D catFactory2D1 = hsp_catiaProfil.OpenEdition();
+
+            Point2D catPoint2D1 = catFactory2D1.CreatePoint(0, r_ah);
+            Point2D catPoint2D2 = catFactory2D1.CreatePoint(r_ab, r_ah);
+            Point2D catPoint2D3 = catFactory2D1.CreatePoint(r_ab, 0);
+            Point2D catPoint2D4 = catFactory2D1.CreatePoint(0, 0);
+
+            Point2D catPoint2D5 = catFactory2D1.CreatePoint((r_ab-r_ib)/2, r_ih+(r_ah-r_ih)/2);
+            Point2D catPoint2D6 = catFactory2D1.CreatePoint(r_ib+(r_ab-r_ib)/2, r_ih+(r_ah-r_ih)/2);
+            Point2D catPoint2D7 = catFactory2D1.CreatePoint(r_ib+ (r_ab - r_ib) / 2, (r_ah-r_ih)/2);
+            Point2D catPoint2D8 = catFactory2D1.CreatePoint((r_ab-r_ib)/2, (r_ah-r_ih)/2);
+
+            Line2D catLine2D1 = catFactory2D1.CreateLine(0, r_ah, r_ab, r_ah);
+            catLine2D1.StartPoint = catPoint2D1;
+            catLine2D1.EndPoint = catPoint2D2;
+
+            Line2D catLine2D2 = catFactory2D1.CreateLine(r_ab, r_ah, r_ab, 0);
+            catLine2D2.StartPoint = catPoint2D2;
+            catLine2D2.EndPoint = catPoint2D3;
+
+            Line2D catLine2D3 = catFactory2D1.CreateLine(r_ab, 0, 0, 0);
+            catLine2D3.StartPoint = catPoint2D3;
+            catLine2D3.EndPoint = catPoint2D4;
+
+            Line2D catLine2D4 = catFactory2D1.CreateLine(0, 0, 0, r_ah);
+            catLine2D4.StartPoint = catPoint2D4;
+            catLine2D4.EndPoint = catPoint2D1;
+
+            Line2D catLine2D5 = catFactory2D1.CreateLine((r_ab-r_ib)/2, r_ih+(r_ah-r_ih)/2, r_ib+(r_ab-r_ib)/2, r_ih+(r_ah-r_ih)/2);
+            catLine2D5.StartPoint = catPoint2D5;
+            catLine2D5.EndPoint = catPoint2D6;
+
+            Line2D catLine2D6 = catFactory2D1.CreateLine(r_ib+(r_ab-r_ib)/2, r_ih+(r_ah-r_ih)/2, r_ib+ (r_ab - r_ib) / 2,(r_ah-r_ih)/2);
+            catLine2D6.StartPoint = catPoint2D6;
+            catLine2D6.EndPoint = catPoint2D7;
+
+            Line2D catLine2D7 = catFactory2D1.CreateLine(r_ib + (r_ab - r_ib) / 2, (r_ah - r_ih) / 2,(r_ab-r_ib)/2,(r_ah-r_ih)/2);
+            catLine2D7.StartPoint = catPoint2D7;
+            catLine2D7.EndPoint = catPoint2D8;
+
+            Line2D catLine2D8 = catFactory2D1.CreateLine((r_ab - r_ib) / 2, (r_ah - r_ih) / 2, (r_ab - r_ib) / 2, r_ih+(r_ah-r_ih)/2);
+            catLine2D8.StartPoint = catPoint2D8;
+            catLine2D8.EndPoint = catPoint2D5;
+
+            hsp_catiaProfil.CloseEdition();
+            hsp_catiaPart.Part.Update();
+        }
+
+        public void ErzeugeQuadratrohr(Double qr_t)
+        {
+            hsp_catiaPart.Part.InWorkObject = hsp_catiaPart.Part.MainBody;
+
+            ShapeFactory catShapeFactory1 = (ShapeFactory)hsp_catiaPart.Part.ShapeFactory;
+            Pad catPad1 = catShapeFactory1.AddNewPad(hsp_catiaProfil, qr_t);
+
+            catPad1.set_Name("Rechteckrohr");
+
+            hsp_catiaPart.Part.Update();
+        }
+
+        public void ErzeugeStab(Double tr)
+        {
+            hsp_catiaPart.Part.InWorkObject = hsp_catiaPart.Part.MainBody;
+
+            ShapeFactory catShapeFactory1 = (ShapeFactory)hsp_catiaPart.Part.ShapeFactory;
+            Pad catPad1 = catShapeFactory1.AddNewPad(hsp_catiaProfil, tr);
+
+            catPad1.set_Name("Stab");
+
+            hsp_catiaPart.Part.Update();
+        }
+
+        public void ErzeugeRohr(Double rtr)
+        {
+            hsp_catiaPart.Part.InWorkObject = hsp_catiaPart.Part.MainBody;
+
+            ShapeFactory catShapeFactory1 = (ShapeFactory)hsp_catiaPart.Part.ShapeFactory;
+            Pad catPad1 = catShapeFactory1.AddNewPad(hsp_catiaProfil, rtr);
+
+            catPad1.set_Name("Rohr");
+
+            hsp_catiaPart.Part.Update();
+        }
+
+        public void ErzeugeBalken(Double t)
+        {
+            hsp_catiaPart.Part.InWorkObject = hsp_catiaPart.Part.MainBody;
+
+            ShapeFactory catShapeFactory1 = (ShapeFactory)hsp_catiaPart.Part.ShapeFactory;
+            Pad catPad1 = catShapeFactory1.AddNewPad(hsp_catiaProfil, t);
+
+            catPad1.set_Name("Balken");
+
+            hsp_catiaPart.Part.Update();
+        }
+    }
+}
---
+++ Vierkantrohr/Catia_Anbindung_GUI/MainWindow.xaml.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Catia_Anbindung_GUI
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();                     
+        }
+
+        private void Btn_Clear_Click(object sender, RoutedEventArgs e)
+        {
+            Felderleeren();
+        }
+
+        private void Felderleeren()
+        {
+            TxtB_hoehe.Text = "0";
+            TxtB_breite.Text = "0";
+            TxtB_tiefe.Text = "0";
+            TxtB_tiefer.Text = "0";
+            TxtB_radius.Text = "0";
+            TxtB_RAhoehe.Text = "0";
+            TxtB_RIhoehe.Text = "0";
+            TxtB_RAbreite.Text = "0";
+            TxtB_RIbreite.Text = "0";
+            TxtB_RAradius.Text = "0";
+            TxtB_RIradius.Text = "0";
+            TxtB_Rtiefe.Text = "0";
+            TxtB_Rtiefer.Text = "0";
+        }
+
+        private void Btn_Erstellen_Click(object sender, RoutedEventArgs e)
+        {
+            Erstellen();
+        }
+
+        public void Erstellen()
+        {
+            try
+            {
+                CatiaCon cc = new CatiaCon();
+                if (cc.CATIALaeuft())
+                {
+                    double h = Convert.ToDouble(TxtB_hoehe.Text);
+                    double b = Convert.ToDouble(TxtB_breite.Text);
+                    double t = Convert.ToDouble(TxtB_tiefe.Text);
+                    double tr = Convert.ToDouble(TxtB_tiefer.Text);
+                    double r = Convert.ToDouble(TxtB_radius.Text);
+                    double r_ah = Convert.ToDouble(TxtB_RAhoehe.Text);
+                    double r_ih = Convert.ToDouble(TxtB_RIhoehe.Text);
+                    double r_ab = Convert.ToDouble(TxtB_RAbreite.Text);
+                    double r_ib = Convert.ToDouble(TxtB_RIbreite.Text);
+                    double qr_t = Convert.ToDouble(TxtB_Rtiefe.Text);
+                    double ra = Convert.ToDouble(TxtB_RAradius.Text);
+                    double ri = Convert.ToDouble(TxtB_RIradius.Text);
+                    double rtr = Convert.ToDouble(TxtB_Rtiefer.Text);
+                    int eins = Convert.ToInt32(TxtB_hoehe.Text);
+                    int zwei = Convert.ToInt32(TxtB_radius.Text);
+                    int drei = Convert.ToInt32(TxtB_RAhoehe.Text);
+                    int vier = Convert.ToInt32(TxtB_RAradius.Text);
+                    int fünf = Convert.ToInt32(ChBo_Radien.IsChecked);
+                    if(h>0 ^ r_ah > r_ih & r_ab > r_ib ^ ra > ri ^ r > 0)
+                    {
+                        cc.ErzeugePart();
+                        cc.ErstelleLeereSkizze();
+                        if (eins > 0 & zwei == 0 & drei == 0 & vier == 0)
+                        {
+                            cc.ErzeugeRechteck(b, h);
+                            cc.ErzeugeBalken(t);
+                        }
+                        if (eins == 0 & zwei > 0 & drei == 0 & vier == 0)
+                        {
+                            cc.ErzeugeKreis(r);
+                            cc.ErzeugeStab(tr);
+                        }
+                        if (eins == 0 & zwei == 0 & drei > 0 & vier == 0)
+                        {
+                            if (fünf > 0)
+                            {
+                                cc.ErzeugeVierkantrohr(r_ah, r_ih, r_ab, r_ib);
+                            }
+                            else
+                            {
+                                cc.ErzeugeRechteckRohr(r_ah, r_ih, r_ab, r_ib);
+                            }
+                            cc.ErzeugeQuadratrohr(qr_t);
+                        }
+                        if (eins == 0 & zwei == 0 & drei == 0 & vier > 0)
+                        {
+                            cc.ErzeugeKreisring(ra, ri);
+                            cc.ErzeugeRohr(rtr);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Äußerer Wert nicht größer als innerer Wert!");
+                    }
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Laufende Catia Application nicht gefunden");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Exception aufgetreten");
+            }
+        }
+
+        private void Pict_Rechteck_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Rechteckauswahl();
+        }
+
+        public void Rechteckauswahl()
+        {
+            Felderleeren();
+            Grid_Profilauswahl.Visibility = Visibility.Hidden;
+            Grid_Vollprofil_Rechteck.Visibility = Visibility.Visible;          
+        }
+
+        private void Btn_Zurück_Click(object sender, RoutedEventArgs e)
+        {
+            Profilauswahl();
+        }
+
+        private void Profilauswahl()
+        {
+            Grid_Profilauswahl.Visibility = Visibility.Visible;
+            Grid_Vollprofil_Rundprofil.Visibility = Visibility.Hidden;
+            Grid_Vollprofil_Rechteck.Visibility = Visibility.Hidden;
+            Grid_Rechteckrohr.Visibility = Visibility.Hidden;
+            Grid_Rohrprofil.Visibility = Visibility.Hidden;
+        }
+
+        public void Rundprofilauswahl()
+        {
+            Felderleeren();
+            Grid_Profilauswahl.Visibility = Visibility.Hidden;
+            Grid_Vollprofil_Rundprofil.Visibility = Visibility.Visible;
+        }
+
+        private void Pict_Rundprofil_MouseLeftButtonDown_1(object sender, MouseButtonEventArgs e)
+        {
+            Rundprofilauswahl();
+        }
+
+        private void Pict_Rechteckrohr_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            RechteckrohrAuswahl();
+        }
+        
+        public void RechteckrohrAuswahl()
+        {
+            Felderleeren();
+            Grid_Profilauswahl.Visibility = Visibility.Hidden;
+            Grid_Rechteckrohr.Visibility = Visibility.Visible;
+        }
+
+        private void Pict_Rohr_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            RohrAuswahl();
+        }
+
+        public void RohrAuswahl()
+        {
+            Felderleeren();
+            Grid_Profilauswahl.Visibility = Visibility.Hidden;
+            Grid_Rohrprofil.Visibility = Visibility.Visible;
+        }       
+    }    
+}
